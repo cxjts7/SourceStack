5bb5b26cd3bd1bfd7130a32d4b1d0b7ab4f55ad2
JF L <1793176487@qq.com>
2021-10-15 12:04:37 +0800

    观察“一起帮”的发布求助功能，试着建立表Problem，包含：
    Id
    Title（标题）
    Content（正文）
    NeedRemoteHelp（需要远程求助）
    Reward（悬赏）
    PublishDateTime（发布时间）……
    请为这些列选择合适的数据类型。

---
+++ SQLQuery3.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Problem] (
+    [Id]              VARCHAR (8)  NOT NULL,
+    [Title]           VARCHAR (12) NOT NULL,
+    [Content]         TEXT         NOT NULL,
+    [NeedRemoteHelp]  BIT          NOT NULL,
+    [Reward]          VARCHAR (10) NOT NULL,
+    [PublishDateTime] DATETIME     NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
