c74496e5f8122aa676a1607ae42e1c2dcf115674
JF L <1793176487@qq.com>
2022-02-13 12:41:09 +0800

    在User表中插入以下四行数据： UserName Password 17bang 1234 Admin NULL Admin-1
    SuperAdmin
    123456
    将Problem表中的Reward全部更新为0
    使用事务，
    删除User表中的全部数据，
    回滚事务，撤销之前的删除行为

---
+++ SQLQuery4.sql
@@ -0,0 +1,12 @@
+﻿INSERT [User](UserName, Password) VALUES(17, 1234);
+SELECT * FROM [User];
+ALTER TABLE [User] ALTER COLUMN Password CHAR(20);
+UPDATE [User] SET UserName = '17bang';
+INSERT [User](UserName, Password) VALUES('Admin', NULL);
+INSERT [User](UserName) VALUES('Admin-1');
+INSERT [User](UserName, Password) VALUES('SuperAdmin', 123456);
+UPDATE [Problem] SET Reward = 0;
+
+BEGIN TRAN
+DELETE [User];
+ROLLBACK
---
+++ SQLQuery5.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE Teacher( 
+Id INT, 
+[Name] NVARCHAR(25), 
+Age INT, 
+Gender BIT 
+) 
+
+CREATE UNIQUE CLUSTERED INDEX IX_Teacher_ID ON Teacher(id)
