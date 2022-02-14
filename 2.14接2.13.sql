cdc51dc58d3008ea0a0aeb5e9a39f11b26927c60
JF L <1793176487@qq.com>
2022-02-14 08:27:05 +0800

    观察一起帮的“关键字”功能，新建Keyword表，要求带一个自增的主键Id，起始值为10，步长为5；并存入若干条数据
    （以下丢失。。。）
    查找没有录入密码的用户
    删除用户名（UserName）中包含“Admin”或者“17bang”字样的用户
    在Problem表中：
    
    给所有悬赏（Reward）大于10的求助标题加前缀：【推荐】
    给所有悬赏大于20且发布时间（Created）在2019年10月10日之后的求助标题加前缀：【加急】
    删除所有标题以中括号【】开头（无论其中有什么内容）的求助
    查找Title中第8个起，字符不为“数据库”且包含了百分号（%）的求助
    在Keyword表中：
    
    找出所有被使用次数（Used）大于10小于50的关键字名称（Name）
    如果被使用次数（Used）小于等于0，或者是NULL值，或者大于100的，将其更新为1
    删除所有使用次数为奇数的Keyword
    注意，上述作业需要自己插入数据进行测试
    
    在User表上的基础上：
    
    添加Id列，让Id成为主键
    添加约束，让UserName不能重复
    在Problem表的基础上：
    
    为NeedRemoteHelp添加NOT NULL约束，再删除NeedRemoteHelp上NOT NULL的约束
    添加自定义约束，让Reward不能小于3

---
+++ 2.14接2.13.sql
@@ -0,0 +1,17 @@
+﻿
+--观察一起帮的“关键字”功能，新建Keywords表，
+--要求带一个自增的主键Id，起始值为10，步长为5；并存入若干条数据
+create table Keywords(
+	[Id] int identity(10, 5),
+	UserName nvarchar(20),
+	Used varchar(20)
+)
+
+insert Keywords(UserName, Used) values('html', 32);
+insert Keywords(UserName, Used) values('css', 13);
+insert Keywords(UserName, Used) values('javascript', 55);
+insert Keywords(UserName, Used) values('sql', 18);
+insert Keywords(UserName, Used) values('c#', 97);
+insert Keywords(UserName, Used) values('Linux', 12);
+
+select * from Keywords
