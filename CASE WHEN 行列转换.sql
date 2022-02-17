b234e358512e3757ab765ffdfbf38453053a8f0b
JF L <1793176487@qq.com>
2022-02-17 20:08:40 +0800

    case when 行列转换

---
+++ CASE WHEN 行列转换.sql
@@ -0,0 +1,9 @@
+﻿select * from Tscore;
+
+select 
+name,
+MAX(case [Subject] when N'C#' then score else 0 end) as N'C#',
+MAX(case [Subject] when N'SQL' then score else 0 end) as N'SQL',
+MAX(case [Subject] when N'JavaScript' then score else 0 end) as N'JavaScript'
+from TSCORE
+group by Name;
